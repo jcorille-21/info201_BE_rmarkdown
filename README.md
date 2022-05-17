6b45c49827d2dbb21916bc76175cf0313f5e4f02
jcorille-21 <jcorille@uw.edu>
2022-05-17 00:07:10 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# info201_BE_rmarkdown
+# INFO 201 Section BE Markdown
+
+For this lab, do the following:
+
+- Fill out the logic in `code.R`
+- Display the variables you created in `code.R` in `index.Rmd` *(hint: source it)*
+- Knit the `index.Rmd` file into an HTML file
---
+++ code.R
@@ -0,0 +1,18 @@
+# Load libraries
+library(dplyr)
+library(usmap)
+library(ggplot2)
+
+# Read data
+incarceration_data <- read.csv('incarceration_trends.csv', stringsAsFactors = F)
+
+# Filter data by...
+# - total population in Washington in 2008
+# - max county population in Washington in 2008
+# - total state populations in 2008
+
+
+# Map data using plot_usmap()
+
+
+
