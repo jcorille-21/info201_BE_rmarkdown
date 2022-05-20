cdf68c8b6a12cf6357c4a2d46514f7fe25de4c5b
jcorille-21 <jcorille@uw.edu>
2022-05-20 16:00:31 -0700

    solutions

--- code.R
+++ code.R
@@ -11,8 +11,29 @@ incarceration_data <- read.csv('incarceration_trends.csv', stringsAsFactors = F)
 # - max county population in Washington in 2008
 # - total state populations in 2008
 
+wa_pop_2008 <- incarceration_data %>%
+  filter(state == 'WA' & year == '2008')
+
+max_wa_pop_2008 <- wa_pop_2008 %>%
+  filter(total_pop == max(total_pop)) %>%
+  pull(total_pop)
+
+us_pop_2008 <- incarceration_data %>%
+  filter(year == '2008') %>%
+  group_by(state) %>%
+  summarize(total_state_pop = sum(total_pop))
 
 # Map data using plot_usmap()
 
+wa_map <- plot_usmap(data = wa_pop_2008,
+                     values = 'total_pop',
+                     include = 'WA',
+                     color = 'white') +
+  scale_fill_continuous(low = 'blue', high = 'red')
+wa_map
 
-
+us_map <- plot_usmap(data = us_pop_2008,
+                     values = 'total_state_pop',
+                     color = 'black') +
+  scale_fill_continuous(low = 'white', high = 'green')
+us_map
